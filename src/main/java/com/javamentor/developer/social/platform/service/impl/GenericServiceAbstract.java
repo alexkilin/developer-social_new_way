c7f2d58083aa146164d1967bbb8d5b9af3597832
Stepanov-Pavel <PStepanov87@mail.ru>
2020-07-21 15:05:52 +0300

    Реализован метод existById() который возвращает true или false если сущность существует по id, добавлена аннтоация @Transactional над методами

--- src/main/java/com/javamentor/developer/social/platform/service/impl/GenericServiceAbstract.java
+++ src/main/java/com/javamentor/developer/social/platform/service/impl/GenericServiceAbstract.java
@@ -3,6 +3,7 @@ package com.javamentor.developer.social.platform.service.impl;
 import com.javamentor.developer.social.platform.dao.impl.GenericDaoAbstract;
 import com.javamentor.developer.social.platform.service.abstracts.GenericService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.Serializable;
 
@@ -14,28 +15,39 @@ public abstract class GenericServiceAbstract<T, PK extends Serializable, R exten
         this.dao = dao;
     }
 
+    @Transactional
     @Override
     public void create(T entity) {
         dao.create(entity);
     }
 
+    @Transactional
     @Override
     public void update(T entity) {
         dao.update(entity);
     }
 
+    @Transactional
     @Override
     public T getById(PK id) {
         return dao.getById(id);
     }
 
+    @Transactional
     @Override
     public void delete(T entity) {
         dao.delete(entity);
     }
 
+    @Transactional
     @Override
     public void deleteById(PK id) {
         dao.deleteById(id);
+    }
+
+    @Transactional
+    @Override
+    public boolean existById(PK id) {
+        return dao.existById(id);
     }
 }
