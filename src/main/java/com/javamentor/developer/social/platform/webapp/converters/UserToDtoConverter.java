bd48a077eb6f5baaaabe5353e5aa00808c733f4b
Dmitriy Gorkov <messer124@gmail.com>
2020-07-24 14:01:02 +0300

    UserConverter

--- src/main/java/com/javamentor/developer/social/platform/webapp/converters/UserToDtoConverter.java
+++
@@ -1,4 +0,0 @@
-package com.javamentor.developer.social.platform.webapp.converters;
-
-public class UserToDtoConverter {
-}
---
+++ src/test/java/com/javamentor/developer/social/platform/resource/user/UserRecourseControllerTest.java
@@ -0,0 +1,7 @@
+package com.javamentor.developer.social.platform.resource.user;
+
+import com.javamentor.developer.social.platform.PlatformApplicationTests;
+
+public class UserRecourseControllerTest extends PlatformApplicationTests {
+
+}
