9a0864fc277b156324c3bde8c3f981f717a01e3b
Stepanov-Pavel <PStepanov87@mail.ru>
2020-07-31 14:17:32 +0300

    Добавлен абстрактный метод getGroupById(). В абстрактный метод getAllGroups() добавлено два аргумента page и size.

--- src/main/java/com/javamentor/developer/social/platform/dao/abstracts/dto/GroupDtoDao.java
+++ src/main/java/com/javamentor/developer/social/platform/dao/abstracts/dto/GroupDtoDao.java
@@ -1,9 +1,12 @@
 package com.javamentor.developer.social.platform.dao.abstracts.dto;
 
+import com.javamentor.developer.social.platform.models.dto.group.GroupDto;
 import com.javamentor.developer.social.platform.models.dto.group.GroupInfoDto;
 
 import java.util.List;
 
 public interface GroupDtoDao {
-    List<GroupInfoDto> getAllGroups();
+    List<GroupInfoDto> getAllGroups(int page, int size);
+
+    GroupDto getGroupById(Long id);
 }
--- src/main/java/com/javamentor/developer/social/platform/service/abstracts/dto/GroupDtoService.java
+++ src/main/java/com/javamentor/developer/social/platform/service/abstracts/dto/GroupDtoService.java
@@ -1,9 +1,12 @@
 package com.javamentor.developer.social.platform.service.abstracts.dto;
 
+import com.javamentor.developer.social.platform.models.dto.group.GroupDto;
 import com.javamentor.developer.social.platform.models.dto.group.GroupInfoDto;
 
 import java.util.List;
 
 public interface GroupDtoService {
-    List<GroupInfoDto> getAllGroups();
+    List<GroupInfoDto> getAllGroups(int page, int size);
+
+    GroupDto getGroupById(Long id);
 }
