ebbbc6b0280ca0ff0fe18a47a93e19eedd06d834
Stepanov-Pavel <PStepanov87@mail.ru>
2020-08-03 15:57:38 +0300

    Добавлен PostDto класс.

---
+++ src/main/java/com/javamentor/developer/social/platform/models/dto/PostDto.java
@@ -0,0 +1,44 @@
+package com.javamentor.developer.social.platform.models.dto;
+
+import com.javamentor.developer.social.platform.models.dto.comment.CommentDto;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@Builder
+public class PostDto {
+
+    @ApiModelProperty(notes = "Автоматически генерируемыЙ ID новости. При создании не указывать, указывать при изменении")
+    private Long id;
+
+    @NotNull
+    @ApiModelProperty(notes = "Заголовок новости, поле не должно быть пустым")
+    private String title;
+
+    @NotNull
+    @ApiModelProperty(notes = "Текст новсти, поле не должно быть пустым")
+    private String text;
+
+    @ApiModelProperty(notes = "Пользователь, добавивший пост")
+    private UserDto userDto;
+
+    @ApiModelProperty(notes = "Дата последнего изменения")
+    private LocalDateTime lastRedactionDate;
+
+    @ApiModelProperty(notes = "Дата добавления поста")
+    private LocalDateTime persistDate;
+
+    @ApiModelProperty(notes = "Медиаконтент поста")
+    private List<MediaPostDto> media;
+
+    @ApiModelProperty(notes = "Коментарии поста")
+    private List<CommentDto> comments;
+}
