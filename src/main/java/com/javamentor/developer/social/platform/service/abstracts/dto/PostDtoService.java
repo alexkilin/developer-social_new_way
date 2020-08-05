e3048dc8b6d59e1f25d56568968570ab1d65a329
Stepanov-Pavel <PStepanov87@mail.ru>
2020-08-05 17:14:46 +0300

    Добавлен абстрактный меод getCommentsByPostId().

--- src/main/java/com/javamentor/developer/social/platform/service/abstracts/dto/PostDtoService.java
+++ src/main/java/com/javamentor/developer/social/platform/service/abstracts/dto/PostDtoService.java
@@ -1,9 +1,12 @@
 package com.javamentor.developer.social.platform.service.abstracts.dto;
 
 import com.javamentor.developer.social.platform.models.dto.PostDto;
+import com.javamentor.developer.social.platform.models.dto.comment.CommentDto;
 
 import java.util.List;
 
 public interface PostDtoService {
     List<PostDto> getPosts();
+
+    List<CommentDto> getCommentsByPostId(Long id);
 }
