6afffae7e5c956b0d0b12da59b81061874a59dac
Stepanov-Pavel <PStepanov87@mail.ru>
2020-08-05 17:14:10 +0300

    Добавлен абстрактный метод getPostsByGroupId().

--- src/main/java/com/javamentor/developer/social/platform/service/abstracts/dto/GroupDtoService.java
+++ src/main/java/com/javamentor/developer/social/platform/service/abstracts/dto/GroupDtoService.java
@@ -2,6 +2,7 @@ package com.javamentor.developer.social.platform.service.abstracts.dto;
 
 import com.javamentor.developer.social.platform.models.dto.group.GroupDto;
 import com.javamentor.developer.social.platform.models.dto.group.GroupInfoDto;
+import com.javamentor.developer.social.platform.models.dto.group.GroupWallDto;
 
 import java.util.List;
 
@@ -9,4 +10,6 @@ public interface GroupDtoService {
     List<GroupInfoDto> getAllGroups(int page, int size);
 
     GroupDto getGroupById(Long id);
+
+    List<GroupWallDto> getPostsByGroupId(Long id, int page, int size);
 }
