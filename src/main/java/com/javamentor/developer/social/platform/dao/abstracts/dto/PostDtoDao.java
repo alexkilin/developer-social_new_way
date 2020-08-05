1ef9249c4a610e68ed0580e59e5e078df9cb0c6f
Stepanov-Pavel <PStepanov87@mail.ru>
2020-08-05 17:05:12 +0300

    Добавлены абстрактные методы getCommentsByPostId(), getMediasByPostId(), getTagsByPostId().

--- src/main/java/com/javamentor/developer/social/platform/dao/abstracts/dto/PostDtoDao.java
+++ src/main/java/com/javamentor/developer/social/platform/dao/abstracts/dto/PostDtoDao.java
@@ -1,9 +1,18 @@
 package com.javamentor.developer.social.platform.dao.abstracts.dto;
 
+import com.javamentor.developer.social.platform.models.dto.MediaPostDto;
 import com.javamentor.developer.social.platform.models.dto.PostDto;
+import com.javamentor.developer.social.platform.models.dto.TagDto;
+import com.javamentor.developer.social.platform.models.dto.comment.CommentDto;
 
 import java.util.List;
 
 public interface PostDtoDao {
     List<PostDto> getPosts();
+
+    List<CommentDto> getCommentsByPostId(Long id);
+
+    List<MediaPostDto> getMediasByPostId(Long id);
+
+    List<TagDto> getTagsByPostId(Long id);
 }
