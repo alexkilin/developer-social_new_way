cd71bbdded601c221feec704a79a067b2dd5d6f8
GDmitriy <Messer124@github.com>
2020-08-05 21:09:15 +0300

    Мелкие исправления

--- src/main/java/com/javamentor/developer/social/platform/dao/impl/dto/UserDtoDaoImpl.java
+++ src/main/java/com/javamentor/developer/social/platform/dao/impl/dto/UserDtoDaoImpl.java
@@ -131,3 +131,4 @@ public class UserDtoDaoImpl implements UserDtoDao {
     }
 
 }
+
---
+++ src/main/java/com/javamentor/developer/social/platform/models/dto/RoleDto.java
@@ -0,0 +1,13 @@
+package com.javamentor.developer.social.platform.models.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RoleDto {
+
+    private String name;
+}
