bf5757b0dcc38efd732768ad375c922678963613
Stepanov-Pavel <PStepanov87@mail.ru>
2020-08-05 17:04:31 +0300

    Добавлен абстарктный метод getPostsByGroupId().

--- src/main/java/com/javamentor/developer/social/platform/dao/abstracts/dto/GroupDtoDao.java
+++ src/main/java/com/javamentor/developer/social/platform/dao/abstracts/dto/GroupDtoDao.java
@@ -2,6 +2,7 @@ package com.javamentor.developer.social.platform.dao.abstracts.dto;
 
 import com.javamentor.developer.social.platform.models.dto.group.GroupDto;
 import com.javamentor.developer.social.platform.models.dto.group.GroupInfoDto;
+import com.javamentor.developer.social.platform.models.dto.group.GroupWallDto;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +11,6 @@ public interface GroupDtoDao {
     List<GroupInfoDto> getAllGroups(int page, int size);
 
     Optional<GroupDto> getGroupById(Long id);
+
+    List<GroupWallDto> getPostsByGroupId(Long id, int page, int size);
 }
