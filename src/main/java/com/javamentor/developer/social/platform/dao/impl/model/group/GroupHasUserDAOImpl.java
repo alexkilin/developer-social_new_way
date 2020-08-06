61b12f2e0b578ff946956ff30a1e271a349f0ba6
Stepanov-Pavel <PStepanov87@mail.ru>
2020-08-06 15:24:05 +0300

    Реализован метод verificationUserInGroup(), запрос которого проверяет наличие записей в БД по переданным параметрам.

--- src/main/java/com/javamentor/developer/social/platform/dao/impl/model/group/GroupHasUserDAOImpl.java
+++ src/main/java/com/javamentor/developer/social/platform/dao/impl/model/group/GroupHasUserDAOImpl.java
@@ -3,8 +3,27 @@ package com.javamentor.developer.social.platform.dao.impl.model.group;
 import com.javamentor.developer.social.platform.dao.abstracts.model.group.GroupHasUserDAO;
 import com.javamentor.developer.social.platform.dao.impl.GenericDaoAbstract;
 import com.javamentor.developer.social.platform.models.entity.group.GroupHasUser;
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Repository
 public class GroupHasUserDAOImpl extends GenericDaoAbstract<GroupHasUser, Long> implements GroupHasUserDAO {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public boolean verificationUserInGroup(Long groupId, Long userId) {
+        Query queryVerifyUserInGroup = (Query) entityManager.createQuery(
+                "SELECT " +
+                        "CASE WHEN COUNT(ghu)>0 THEN true ELSE false END " +
+                    "FROM GroupHasUser ghu " +
+                    "WHERE ghu.user.userId = :paramUserId AND ghu.group.id = :paramGroupId")
+                .setParameter("paramUserId", userId)
+                .setParameter("paramGroupId", groupId);
+        return (boolean) queryVerifyUserInGroup.unwrap(Query.class).getSingleResult();
+    }
 }
