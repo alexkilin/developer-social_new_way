d9a2e5d5d22e4cd93dd582576cedeb723edbad05
Stepanov-Pavel <PStepanov87@mail.ru>
2020-08-15 18:37:56 +0300

    Добавлено множество datasets. Реализованы все методы тестирования.

--- src/test/java/com/javamentor/developer/social/platform/controllers/GroupControllerTest.java
+++ src/test/java/com/javamentor/developer/social/platform/controllers/GroupControllerTest.java
@@ -12,37 +12,66 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DataSet(value = {
-        "datasets/group/groups.yml",
+        "datasets/group/Group.yml",
         "datasets/audio/usersAudioTest/User.yml",
         "datasets/group/GroupCategory.yml",
         "datasets/audio/usersAudioTest/Active.yml",
         "datasets/audio/usersAudioTest/Role.yml",
-        "datasets/audio/usersAudioTest/Status.yml"
+        "datasets/audio/usersAudioTest/Status.yml",
+        "datasets/group/GroupHasUser.yml",
+        "datasets/post/media.yml",
+        "datasets/post/post_media.yml",
+        "datasets/post/post_tags.yml",
+        "datasets/post/posts.yml",
+        "datasets/post/tags.yml",
+        "datasets/group/GroupWal.yml"
 },
         cleanBefore = true,
         cleanAfter = true)
-public class GroupControllerTest extends AbstractIntegrationTest {
+class GroupControllerTest extends AbstractIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    public void getAllGroups() throws Exception {
-        this.mockMvc.perform(get("/api/groups/all"))
+    void getAllGroups() throws Exception {
+        this.mockMvc.perform(get("/api/groups/all?page=1&size=3"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.length()").value(1));
+                .andExpect(jsonPath("$.length()").value(3));
     }
 
     @Test
-    void showGroup() {
+    void showGroup() throws Exception {
+        this.mockMvc.perform(get("/api/groups/{id}", "1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("id").value(1))
+                .andExpect(jsonPath("name").value("JAVA IS 0"))
+                .andExpect(jsonPath("lastRedactionDate").value("2020-08-13T11:46:35.493"))
+                .andExpect(jsonPath("persistDate").value("2020-08-13T11:46:35.493"))
+                .andExpect(jsonPath("linkSite").value("www.groupsite0.ru"))
+                .andExpect(jsonPath("groupCategory").value("Programming"))
+                .andExpect(jsonPath("ownerFio").value("LastNameUser0 Admin0"))
+                .andExpect(jsonPath("description").value("This is a description of the group #0"));
     }
 
     @Test
-    void showGroupWall() {
+    void showGroupWall() throws Exception {
+        this.mockMvc.perform(get("/api/groups/{id}/posts?page=1&size=2", "1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2));
     }
 
     @Test
-    void findGroupByName() {
+    void findGroupByName() throws Exception {
+        this.mockMvc.perform(get("/api/groups/name?name=JAVA IS 0"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("id").value(1))
+                .andExpect(jsonPath("name").value("JAVA IS 0"))
+                .andExpect(jsonPath("groupCategory").value("Programming"))
+                .andExpect(jsonPath("subscribers").value(1));
     }
 }
