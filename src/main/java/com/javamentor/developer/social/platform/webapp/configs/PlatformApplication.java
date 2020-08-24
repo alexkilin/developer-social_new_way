73f213b7cadad4714c16ca588ca97f560a54c027
Stepanov-Pavel <PStepanov87@mail.ru>
2020-08-24 12:25:56 +0300

    Добавлена аннотация @EnableSwagger2.

--- src/main/java/com/javamentor/developer/social/platform/webapp/configs/PlatformApplication.java
+++ src/main/java/com/javamentor/developer/social/platform/webapp/configs/PlatformApplication.java
@@ -5,15 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ComponentScan("com.javamentor.developer.social.platform")
 @EntityScan("com.javamentor.developer.social.platform.models.entity")
+@EnableSwagger2
 public class PlatformApplication {
 
     public static void main(String[] args){
