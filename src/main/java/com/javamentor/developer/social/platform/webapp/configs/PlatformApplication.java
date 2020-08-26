8a9b32d621886d466cd8f0990eb467e6e1ed9515
Stepanov-Pavel <PStepanov87@mail.ru>
2020-08-26 19:14:22 +0300

    Удалена аннотация @EnableSwagger2 за ненадобнастью.

--- src/main/java/com/javamentor/developer/social/platform/webapp/configs/PlatformApplication.java
+++ src/main/java/com/javamentor/developer/social/platform/webapp/configs/PlatformApplication.java
@@ -5,12 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ComponentScan("com.javamentor.developer.social.platform")
 @EntityScan("com.javamentor.developer.social.platform.models.entity")
-@EnableSwagger2
 public class PlatformApplication {
 
     public static void main(String[] args){
