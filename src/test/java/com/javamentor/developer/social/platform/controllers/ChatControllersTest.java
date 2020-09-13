e14c1c444b87adb796916d1ae37749b1f7b595ae
pavel <pashamalkin@mail.ru>
2020-09-14 00:21:26 +0300

    Initial Commit

---
+++ src/test/java/com/javamentor/developer/social/platform/controllers/ChatControllersTest.java
@@ -0,0 +1,54 @@
+package com.javamentor.developer.social.platform.controllers;
+
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.google.gson.Gson;
+import com.javamentor.developer.social.platform.AbstractIntegrationTest;
+import com.javamentor.developer.social.platform.models.dto.UserDto;
+import com.javamentor.developer.social.platform.models.dto.chat.ChatDto;
+import com.javamentor.developer.social.platform.models.dto.comment.CommentDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@DataSet(value = {
+        "datasets/postcommentset/comment/Comment.yml",
+        "datasets/postcommentset/comment/PostComment.yml",
+        "datasets/postcommentset/post/media.yml",
+        "datasets/postcommentset/post/post_media.yml",
+        "datasets/postcommentset/post/post_tags.yml",
+        "datasets/postcommentset/post/posts.yml",
+        "datasets/postcommentset/post/tags.yml",
+        "datasets/postcommentset/user/Active.yml",
+        "datasets/postcommentset/user/Role.yml",
+        "datasets/postcommentset/user/Status.yml",
+        "datasets/postcommentset/user/User.yml"
+}, cleanBefore = true, cleanAfter = true)
+public class ChatControllersTest extends AbstractIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void createGroupChat() throws Exception {
+
+        ChatDto chatDto = ChatDto.builder()
+                .id(1L).image("image").lastMessage("lastik").title("Tit").type("groupChats").active("1")
+                .build();
+
+        String json = new Gson().toJson(chatDto);
+
+
+        this.mockMvc.perform(post( "/api/user/chat/group/create")
+        .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").isNotEmpty());
+    }
+}
