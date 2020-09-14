a4a5662dd466fd18fe7afda365db175cbc0075be
pavel <pashamalkin@mail.ru>
2020-09-14 15:09:28 +0300

    Initial Commit

---
+++ src/main/java/com/javamentor/developer/social/platform/webapp/converters/GroupChatConverter.java
@@ -0,0 +1,40 @@
+package com.javamentor.developer.social.platform.webapp.converters;
+
+
+import com.javamentor.developer.social.platform.models.dto.chat.ChatDto;
+import com.javamentor.developer.social.platform.models.entity.chat.GroupChat;
+import com.javamentor.developer.social.platform.models.entity.user.User;
+import com.javamentor.developer.social.platform.service.abstracts.model.user.UserService;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.HashSet;
+import java.util.Set;
+
+@Mapper(componentModel = "spring")
+public abstract class GroupChatConverter {
+
+    @Autowired
+    private UserService userService;
+
+    @Mapping(source = "chatDto.title", target = "title")
+    @Mapping(source = "chatDto.image", target = "image")
+    @Mapping(source = "userId", target = "users", qualifiedByName = "addUserToChat")
+    public abstract GroupChat chatToGroupChat(ChatDto chatDto, Long userId);
+
+
+    @Named("addUserToChat")
+    public Set<User> addUserToChat(Long userId) {
+      User user = userService.getById(userId);
+      Set<User> userSet = new HashSet<>();
+      userSet.add(user);
+      return userSet;
+    }
+
+    @Mapping(source = "groupChat.title", target = "title")
+    @Mapping(source = "groupChat.image", target = "image")
+    @Mapping(source = "groupChat.id", target = "id")
+    public abstract ChatDto groupChatToChatDto(GroupChat groupChat);
+
+}
