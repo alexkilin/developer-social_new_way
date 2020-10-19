b54edd2d956e7b3a0c9262a03dd1f641ad936a69
Andrey Tokarev <tokarevandy@gmail.com>
2020-10-19 13:04:21 +0300

    заменил метод обновления пользователя в UserController более совершенным из UserControllerV2

--- src/main/java/com/javamentor/developer/social/platform/service/impl/model/user/UserServiceImpl.java
+++ src/main/java/com/javamentor/developer/social/platform/service/impl/model/user/UserServiceImpl.java
@@ -12,7 +12,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.HashSet;
