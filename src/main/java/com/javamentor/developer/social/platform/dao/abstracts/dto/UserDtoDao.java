58b0e742a9707721d3e80208af2390e07bf62633
Danil Novikov <danil.novikov.03@gmail.com>
2020-11-11 15:19:52 +0300

    Update UserDtoDao.java

--- src/main/java/com/javamentor/developer/social/platform/dao/abstracts/dto/UserDtoDao.java
+++ src/main/java/com/javamentor/developer/social/platform/dao/abstracts/dto/UserDtoDao.java
@@ -2,11 +2,8 @@ package com.javamentor.developer.social.platform.dao.abstracts.dto;
 
 
 import com.javamentor.developer.social.platform.models.dto.LanguageDto;
-<<<<<<< src/main/java/com/javamentor/developer/social/platform/dao/abstracts/dto/UserDtoDao.java
 import com.javamentor.developer.social.platform.models.dto.UserFriendDto;
-=======
 import com.javamentor.developer.social.platform.models.dto.users.UserDto;
->>>>>>> src/main/java/com/javamentor/developer/social/platform/dao/abstracts/dto/UserDtoDao.java
 
 import java.util.List;
 import java.util.Optional;
