ef46597bae33239b4c26a957baf64b8a4a0eb59f
Danil Novikov <danil.novikov.03@gmail.com>
2020-11-11 15:20:46 +0300

    Update UserDtoDaoImpl.java

--- src/main/java/com/javamentor/developer/social/platform/dao/impl/dto/UserDtoDaoImpl.java
+++ src/main/java/com/javamentor/developer/social/platform/dao/impl/dto/UserDtoDaoImpl.java
@@ -2,11 +2,8 @@ package com.javamentor.developer.social.platform.dao.impl.dto;
 
 import com.javamentor.developer.social.platform.dao.abstracts.dto.UserDtoDao;
 import com.javamentor.developer.social.platform.models.dto.LanguageDto;
-<<<<<<< src/main/java/com/javamentor/developer/social/platform/dao/impl/dto/UserDtoDaoImpl.java
 import com.javamentor.developer.social.platform.models.dto.UserFriendDto;
-=======
 import com.javamentor.developer.social.platform.models.dto.users.UserDto;
->>>>>>> src/main/java/com/javamentor/developer/social/platform/dao/impl/dto/UserDtoDaoImpl.java
 import org.hibernate.query.Query;
 import org.hibernate.transform.ResultTransformer;
 import org.springframework.stereotype.Repository;
