e92322e242d582050ca08ee0dae7cfdc9074b03d
Danil Novikov <danil.novikov.03@gmail.com>
2020-11-11 15:21:27 +0300

    Update UserDtoService.java

--- src/main/java/com/javamentor/developer/social/platform/service/abstracts/dto/UserDtoService.java
+++ src/main/java/com/javamentor/developer/social/platform/service/abstracts/dto/UserDtoService.java
@@ -1,10 +1,7 @@
 package com.javamentor.developer.social.platform.service.abstracts.dto;
 
-<<<<<<< src/main/java/com/javamentor/developer/social/platform/service/abstracts/dto/UserDtoService.java
 import com.javamentor.developer.social.platform.models.dto.UserFriendDto;
-=======
 import com.javamentor.developer.social.platform.models.dto.users.UserDto;
->>>>>>> src/main/java/com/javamentor/developer/social/platform/service/abstracts/dto/UserDtoService.java
 
 import java.util.List;
 import java.util.Optional;
