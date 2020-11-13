1853792d50790ea7ddf08b56cb890c220533540e
unknown <danil.novikov.03@gmail.com>
2020-11-13 13:42:10 +0300

    change Id to id, add annotations

--- src/main/java/com/javamentor/developer/social/platform/dao/impl/dto/PostDtoDaoImpl.java
+++ src/main/java/com/javamentor/developer/social/platform/dao/impl/dto/PostDtoDaoImpl.java
@@ -83,7 +83,7 @@ public class PostDtoDaoImpl implements PostDtoDao {
                         List<MediaPostDto> mediaPostDtoList = new ArrayList<>();
                         if (objects[14] != null) {
                             MediaPostDto mediaPostDto = MediaPostDto.builder()
-                                    .Id((Long) objects[14])
+                                    .id((Long) objects[14])
                                     .userId((Long) objects[15])
                                     .mediaType(objects[16] == null ? "null" : objects[16].toString())
                                     .url((String) objects[17])
@@ -196,7 +196,7 @@ public class PostDtoDaoImpl implements PostDtoDao {
                         List<MediaPostDto> mediaPostDtoList = new ArrayList<>();
                         if (objects[14] != null) {
                             MediaPostDto mediaPostDto = MediaPostDto.builder()
-                                    .Id((Long) objects[14])
+                                    .id((Long) objects[14])
                                     .userId((Long) objects[15])
                                     .mediaType(objects[16] == null ? "null" : objects[16].toString())
                                     .url((String) objects[17])
@@ -309,7 +309,7 @@ public class PostDtoDaoImpl implements PostDtoDao {
                         List<MediaPostDto> mediaPostDtoList = new ArrayList<>();
                         if (objects[14] != null) {
                             MediaPostDto mediaPostDto = MediaPostDto.builder()
-                                    .Id((Long) objects[14])
+                                    .id((Long) objects[14])
                                     .userId((Long) objects[15])
                                     .mediaType(objects[16] == null ? "null" : objects[16].toString())
                                     .url((String) objects[17])
