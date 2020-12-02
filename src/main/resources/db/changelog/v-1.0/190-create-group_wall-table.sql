abf56baf8b11aa876f2ea82b680ea5c52d3f42f6
Tsovak <tsovakpalakian@gmail.com>
2020-12-03 02:31:17 +0300

    added loadData

--- src/main/resources/db/changelog/v-1.0/190-create-group_wall-table.sql
+++ src/main/resources/db/changelog/v-1.0/190-create-group_wall-table.sql
@@ -1,5 +1,5 @@
 create table group_wall (
-   group_id int8 not null,
+    group_id int8 not null,
     post_id int8 not null,
     primary key (group_id, post_id)
 )
@@ -15,13 +15,6 @@ alter table group_wall
    add constraint FKhde1qpuipv4ouvqw02gxruc2h
    foreign key (group_id)
    references groups
-
-next
-
-alter table group_wall
-   add constraint FKqkll3lqcxuse5558nxawjgmp4
-   foreign key (post_id)
-   references posts
 
 next
 
