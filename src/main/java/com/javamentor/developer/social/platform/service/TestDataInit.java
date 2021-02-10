6a3d2ae9d3e169b85aee15956b7edd3b95d4649a
Rogovolod <Rogovolod@gmail.com>
2021-02-10 20:36:54 +0900

    +

--- src/main/java/com/javamentor/developer/social/platform/service/TestDataInit.java
+++ src/main/java/com/javamentor/developer/social/platform/service/TestDataInit.java
@@ -1,5 +1,26 @@
 package com.javamentor.developer.social.platform.service;
 
+import com.javamentor.developer.social.platform.service.abstracts.model.user.RoleService;
+import com.javamentor.developer.social.platform.service.abstracts.model.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+/**
+ * В этом классе необходимо выполнить инициализацию.
+ * И заполнить базу данных случайными значениями.
+ * Например:
+ * - вызвать все методы по созданию пользователей и ролей
+ *  createUser
+ *  createChat - передать пользователя
+ *  createRole - передать пользоваться
+ *  и тд.
+ */
 public class TestDataInit {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    RoleService roleService;
 
 }
--- src/main/java/com/javamentor/developer/social/platform/webapp/configs/TestDataInitializer.java
+++ src/main/java/com/javamentor/developer/social/platform/webapp/configs/TestDataInitializer.java
@@ -1,12 +1,18 @@
 package com.javamentor.developer.social.platform.webapp.configs;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConditionalOnMissingClass({"org.springframework.boot.test.context.SpringBootTest"})
-public class TestDataInitializer {
+public class TestDataInitializer implements CommandLineRunner {
 
+/**
+*    Здесь мы должны вызвать метод TestDataInit
+*/
+    @Override
+    public void run(String... args) throws Exception {
 
-
+    }
 }
